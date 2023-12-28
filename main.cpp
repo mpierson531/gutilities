6c4ee4c736ed8c63c529b15f08010b34cd3f70f9
Micah Pierson <micah.pierson@outlook.com>
2023-12-27 23:06:14 -0700

    Added a couple little examples of gutils::Result and gutils::Array

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include "gutilities/gutils/Array.hpp"
 #include "gutilities/gio/Console.hpp"
-using gutils::Array;
-using gio::Console;
+#include "gutilities/gutils/Result.hpp"
 
 #include <string>
 using std::string;
 
-int main() {
-    Console console;
+void arrayIteration() {
+    gio::Console console;
 
-    Array<string> gutilArray { "Hello", " ", "World", "!" };
+    gutils::Array<string> gutilArray { "Hello", " ", "World", "!" };
+
+    console.println("Iterating gutils::Array!");
 
     for (const string& s : gutilArray) {
         console.print(s);
     }
 
     console.println();
+}
+
+gutils::Result<string, string> constructResultTest() {
+    if (!false) {
+        return gutils::Result<string, string>::errOf("nullptr is false!");
+    }
+
+    return gutils::Result<string, string>::okOf("nullptr is true!");
+}
+
+void resultTest() {
+    gio::Console console;
+
+    auto result = constructResultTest();
+
+    console.println("Testing a result!");
+
+    if (!result) {
+        console.println("result was an error!");
+    }
+}
+
+int main() {
+    gio::Console console;
+
+    arrayIteration();
+
+    console.println();
+
+    resultTest();
 }
